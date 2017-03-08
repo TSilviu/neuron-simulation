72dd6e9631815064b80fb4165e156bd3a79c0d4b
Silviu Tugulan <t.silviu94@gmail.com>
2017-03-08 17:57:16 +0000

    Progressing
    Created plot for q1 with proper labels;
    Simulated the integrate and fire model with 0.1nA smaller than the minimum value and saved the plot

--- integrate_and_fire.py
+++ integrate_and_fire.py
@@ -8,7 +8,7 @@ Vr = -70;
 Tm = 10;
 Rm = 10;
 Vt = -40;
-Ie = 3.1;
+Ie = 2.9;
 
 # 
 
@@ -21,7 +21,7 @@ def apply_euler(dt, clr):
 
 	ts = [t]
 	vs = [v]
-	while t <= 3:
+	while t <= 1:
 		t = t + dt
 		v = v + df(v,t)
 		if(v >= Vt):
@@ -29,11 +29,11 @@ def apply_euler(dt, clr):
 		ts.append(t)
 		vs.append(v)
 	plt.plot(ts,vs,color=clr)
-
+	
 clr = 'b'
 apply_euler(0.01, clr)
 
 plt.xlabel("t (seconds)")
 plt.ylabel("v(t)")
-plt.title("Euler solution with dt=0.01")
+plt.title("Simulation of the integrate and fire model with Ie smaller than the minimum value.")
 plt.show()
