32235a7abd508baf16aafc8bc551be624153e02a
Silviu Tugulan <t.silviu94@gmail.com>
2017-03-08 23:05:58 +0000

    Completed q4

--- integrate_and_fire.py
+++ integrate_and_fire.py
@@ -8,32 +8,45 @@ Vr = -70e-3;
 Tm = 10;
 Rm = 10e6;
 Vt = -40e-3;
-Ie = 3.1e-9;
+Ie = 2.0e-9;
 
 # 
 
-def df(v,t):
-	return (El - v + Rm * Ie)/Tm
+def df(v,t,i):
+	return (El - v + Rm * i)/Tm
 
 def apply_euler(dt, clr):
 	t = 0
 	v = Vr
+	i = Ie
 
 	ts = [t]
 	vs = [v]
-	while t <= 1:
-		t = t + dt
-		v = v + df(v,t)
-		if(v >= Vt):
-			v = Vr
-		ts.append(t)
-		vs.append(v)
-	plt.plot(ts,vs,color=clr)
+	Is = []
+	spikes = []
+
+	while i <= 5.1e-9:	
+		spikes_count = 0
+		while t <= 1:
+			t = t + dt
+			v = v + df(v,t,i)
+			if(v >= Vt):
+				v = Vr
+				spikes_count = spikes_count + 1
+			ts.append(t)
+			vs.append(v)
+		Is.append(i)
+		spikes.append(spikes_count)
+		i = i + 0.1e-9
+		t = 0
+		v = Vr
+	# plt.plot(ts,vs,color=clr)
+	plt.plot(Is,spikes,color=clr)
 	
 clr = 'b'
 apply_euler(0.001, clr)
 
-plt.xlabel("t (seconds)")
-plt.ylabel("v(t)")
-plt.title("Simulation of the integrate and fire model with Ie smaller than the minimum value.")
+plt.xlabel("Current(nA)")
+plt.ylabel("Firing rate - f(Current)")
+plt.title("Increasing the amplitude while counting the number of spikes produced.")
 plt.show()
